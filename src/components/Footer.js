5e55b951df7feb6dda04cdaab74ba032837b402e
Joshua Essex <essexjosh@gmail.com>
2019-08-16 14:01:44 -0700

    Updates the footer text to match recidiviz.org. (Recidiviz/pulse-dashboard#48)
    GitOrigin-RevId: ef295b63aca4e5d194946e028d9aec62de6164ef

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => (
   <footer className="bdT ta-c p-30 lh-0 fsz-sm c-grey-600">
-    <span>Copyright © 2019 <a href="https://www.recidiviz.org/" _target="_blank" title="Recidiviz">Recidiviz</a>. </span>
-    <span>Based on Adminator template designed by <a href="https://colorlib.com" target="_blank" title="Colorlib">Colorlib</a>. All rights reserved.</span>
+    <span>© 2019 <a href="https://www.recidiviz.org/" _target="_blank" title="Recidiviz">Recidiviz</a>. </span>
+    <span>All Rights Reserved.</span>
   </footer>
 );
 
