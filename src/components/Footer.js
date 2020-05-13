703923469adc5d2d47162dae422c35f62df003f7
Joshua Essex <essexjosh@gmail.com>
2020-05-13 13:07:55 -0600

    Updates copyright year in footer. (Recidiviz/pulse-dashboard#334)
    GitOrigin-RevId: 2a3491b70c0c2243927612a8457ab4264e888c87

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="bdT ta-c p-30 lh-0 fsz-sm c-grey-600">
-    <span>© 2019 <a href="https://www.recidiviz.org/" _target="_blank" title="Recidiviz">Recidiviz</a>. All Rights Reserved.</span>
+    <span>© 2020 <a href="https://www.recidiviz.org/" _target="_blank" title="Recidiviz">Recidiviz</a>. All Rights Reserved.</span>
   </footer>
 );
 
