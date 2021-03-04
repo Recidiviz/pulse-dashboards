191ce19163270307a4930cfb46206ddf0c15c9de
Alexa Batino <37125652+alexabatino@users.noreply.github.com>
2021-03-04 10:05:53 -0500

    [US_ND] Update FTR referrals by LSIR chart (Recidiviz/pulse-dashboard#805)
    * Add new bucket for individuals with no LSIR score
    
    * Move 'No Score' to the left of the graph
    
    * PR feedback
    
    GitOrigin-RevId: a6e14cf4174a3aa96b845c13e5a17f2978ef0969

--- src/components/charts/programming/FtrReferralsByLsir.js
+++ src/components/charts/programming/FtrReferralsByLsir.js
@@ -46,8 +46,9 @@ import { metricTypePropType } from "../propTypes";
 import { METRIC_TYPES } from "../../constants";
 
 const chartId = "ftrReferralsByLsir";
+const chartLabels = ["No Score", "0-23", "24-29", "30-38", "39+"];
 
-const lsirScoreBuckets = ["0-23", "24-29", "30-38", "39+"];
+const lsirScoreBuckets = ["NOT_ASSESSED", "0-23", "24-29", "30-38", "39+"];
 const lsirDefaults = reduce(
   (acc, lsir) => ({
     ...acc,
@@ -123,7 +124,6 @@ const FtrReferralsByLsir = ({
     lsirScoreBuckets
   );
 
-  const chartLabels = lsirScoreBuckets;
   const ftrReferralCounts = referralsByAgeCounts;
   const ftrReferralProportions = referralsByAgeProportions;
   const stateSupervisionCounts = supervisionByAgeCounts;
