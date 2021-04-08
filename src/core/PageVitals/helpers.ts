77b028f4820dd78f5f3d055630cf3f33bd279a7c
Anthony Kesich <anthony@recidiviz.org>
2021-04-08 10:28:22 -0500

    Filter vitals summary data for sub-pages (Recidiviz/pulse-dashboard#997)
    GitOrigin-RevId: 6fa838a0c2a1f0e5cda3fb23808c0557c3754660

--- src/core/PageVitals/helpers.ts
+++ src/core/PageVitals/helpers.ts
@@ -91,19 +91,11 @@ export function getEntitySummaries(
   currentEntitySummary: VitalsSummaryRecord;
   childEntitySummaryRows: VitalsSummaryTableRow[];
 } {
-  // The data is configured such that when entityId === parentEntityId,
-  // the row contains summary data for the VitalsSummaryCards for the
-  // current/parent entity
   const currentEntitySummary = vitalsSummaries.find(
-    (d) => d.entityId === currentEntityId && d.parentEntityId === d.entityId
+    (d) => d.entityId === currentEntityId
   ) as VitalsSummaryRecord;
-  // When entityId !== parentEntityId, the row contains summary data
-  // for the VitalsSummaryTable for the child entities
   const childEntitySummaryRows = vitalsSummaries
-    .filter(
-      (d) =>
-        d.parentEntityId === currentEntityId && d.parentEntityId !== d.entityId
-    )
+    .filter((d) => d.parentEntityId === currentEntityId)
     .map((d) => {
       const { entityId, entityName, entityType, ...attrs } = d;
       return {
