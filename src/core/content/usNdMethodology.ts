ecbf3954f39fc3307e986f3e6c7f0c9c97f7df89
Daniela Schiano di Cola <daniela@recidiviz.org>
2021-04-21 17:49:05 -0400

    Update methodolog copy (Recidiviz/pulse-dashboard#1029)
    GitOrigin-RevId: 93b4a7db38e17ad18afd60ceb7cb8c4c20265fb7

--- src/core/content/usNdMethodology.ts
+++ src/core/content/usNdMethodology.ts
@@ -19,77 +19,45 @@ import { ViewMethodology } from "../models/types";
 export const US_ND: ViewMethodology = {
   vitals: {
     title: "At a Glance",
-    description: `The "At a Glance" page provides a snapshot of agency \
-    performance on a set of key metrics to proactively identify potential \
-    resource constraints and focus attention on operational successes and \
-    areas for improvement.`,
+    description: `The “At a Glance” page provides a snapshot of agency performance on a set of key metrics to proactively identify potential resource constraints and focus attention on operational successes and areas for improvement.`,
     content: [
       {
         heading: "Percentage Calculations",
-        content: `\
-        <p class="Methodology__block--content">\
-        The numerator for each percentage calculation is the number of people \
-        who meet following criteria on a given day:</p>\
+        content: `
+        <p class="Methodology__block--content">
+        The numerator for each percentage calculation is the number of people who meet the following criteria on a given day:
+        </p>
         <ul class="Methodology__block--content">
           <li><b>Overall:</b> The sum of all the counts below.</li>
-          <li><b>Timely discharge:</b> Number of people on supervision who are \
-          not past their projected supervision completion date.
+          <li><b>Timely discharge:</b> Number of people on supervision who are not past their projected supervision completion date.
           </li>
-          <li><b>Timely FTR enrollment:</b> Number of people on supervision \
-          who do not have a Pending FTR participation status.
-          </li>
-          <li><b>Timely contacts:</b> The sum of all the counts below. \
+          <li><b>Timely contacts:</b> Sum of the following:
             <ul class="Methodology__block--content">
-            <li>Number of people on Minimum Supervision who have had a \
-            face-to-face contact within the last 90 days and a home visit \
-            within 90 days of their supervision start.</li>
-            <li>Number of people on Medium Supervision who have had a \
-            face-to-face contact within the last 60 days, a home visit within \
-            90 days of their supervision start, and a home visit within the \
-            last 365 days.</li>
-            <li>Number of people on Maximum Supervision who have had a \
-            face-to-face contact within the last 30 days, a home visit within \
-            90 days of their supervision start, and a home visit within the \
-            last 365 days.</li>
+            <li>Number of people on Minimum Supervision who have had a face-to-face contact within the last 90 days and a home visit within 90 days of their supervision start.</li>
+            <li>Number of people on Medium Supervision who have had a face-to-face contact within the last 60 days, a home visit within 90 days of their supervision start, and a home visit within the last 365 days.</li>
+            <li>Number of people on Maximum Supervision who have had a face-to-face contact within the last 30 days, a home visit within 90 days of their supervision start, and a home visit within the last 365 days.</li>
             </ul>
           </li>
-          <li><b>Timely risk assessments:</b> Number of people on supervision \
-          who have had a risk assessment completed within the last 212 days, or\
-           had an assessment within the first 30 days of supervision, if they have been on supervision for fewer than 212 days.
+          <li><b>Timely risk assessments:</b> Number of people on supervision who have had a risk assessment completed within the last 212 days, or had an assessment within the first 30 days of supervision, if they have been on supervision for fewer than 212 days. This excludes all people on supervision for NC, IC-OUT, and diversion.
           </li>
-        </ul>\
-        <p class="Methodology__block--content">\
-        The denominator of each rate calculation is all people on supervision \
-        in a given day for a given region. For example, the denominators of \
-        the percentages for Oakes Office represent the total number of people \
-        on supervision in Oakes.</p>`,
+        </ul>
+        <p class="Methodology__block--content">
+        The denominator of each rate calculation is all people on supervision in a given day for a given region. For example, the denominators of the percentages for Oakes Office represent the total number of people on supervision in Oakes.
+        </p>`,
       },
       {
         heading: "Over-time Calculations",
-        content: `\
+        content: `
         <ul class="Methodology__block--content">
-          <li><b>"Current Performance"</b> takes the numerator and denominator \
-          for the "Data last updated" date. For example, if the data was last \
-          updated on 3/31/21, the timely discharge performance would be \
-          represented by the total number of people with a projected \
-          supervision completion date before 3/31/21 divided by the total \
-          number of people on supervision on 3/31/21. </li>
-          <li><b>Rolling 7-day average:</b> Number of people on supervision who are \
-          not past their projected supervision completion date.
+          <li><b>"Current Performance"</b> takes the numerator and denominator for the “Data last updated” date. For example, if the data was last updated on 3/31/21, the timely discharge performance would be represented by the total number of people with a projected supervision completion date before 3/31/21 divided by the total number of people on supervision on 3/31/21.
           </li>
-          <li><b>Over-time chart:</b> The bars represent the \
-          "Current Performance" as of a given day, and the trendline on a \
-          given day represents the rolling 7-day average.
+          <li><b>Rolling 30-day average:</b> On a given day, the rolling 30-day represents the average of the Current Performance percentage for the 30 days prior.
           </li>
-          <li><b>7D change:</b> This percentage displays the difference \
-          between the rolling 7-day average on the "Data last updated" date \
-          and the rolling 7-day average of 7 days prior to the \
-          "Data last updated" date.
+          <li><b>Over-time chart:</b> The bars represent the “Current Performance” as of a given day, and the trendline on a given day represents the rolling 30-day average.
           </li>
-          <li><b>28D change:</b>This percentage displays the difference \
-          between the rolling 7-day average on the "Data last updated" date \
-          and the rolling 7-day average of 7 days prior to the \
-          "Data last updated" date.
+          <li><b>30D change:</b> This percentage displays the difference between the rolling 30-day average on the “Data last updated” date and the rolling 30-day average of 30 days prior to the “Data last updated” date.
+          </li>
+          <li><b>90D change:</b>This percentage displays the difference between the rolling 30-day average on the “Data last updated” date and the rolling 30-day average of 90 days prior to the “Data last updated” date.
           </li>
         </ul>`,
       },
