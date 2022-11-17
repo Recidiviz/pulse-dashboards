e6ef4c8c078106a51eac78ffae0c76a238fd1f95
Anthony Kesich <anthony@recidiviz.org>
2022-11-17 12:06:28 -0600

    [Workflows] Add type for SCCP record (Recidiviz/recidiviz-dashboards#2689)
    GitOrigin-RevId: f6c8bc43a0f89a6db849b7e9b90b751e7feac8cf

---
+++ src/WorkflowsStore/Opportunity/UsMeSCCPReferralRecord.ts
@@ -0,0 +1,52 @@
+/*
+ * Recidiviz - a data platform for criminal justice reform
+ * Copyright (C) 2022 Recidiviz, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * =============================================================================
+ */
+
+import { cloneDeep } from "lodash";
+
+import { TransformFunction } from "../subscriptions";
+import { fieldToDate } from "../utils";
+import { WithCaseNotes } from "./types";
+
+export type UsMeSCCPReferralRecord = {
+  stateCode: string;
+  externalId: string;
+  criteria: {
+    usMeMinimumOrCommunityCustody: { custodyLevel: string };
+    usMeServedXPortionOfSentence: { eligibleDate: Date };
+    usMeXMonthsRemainingOnSentence: { eligibleDate: Date };
+  };
+} & WithCaseNotes;
+
+export const transformReferral: TransformFunction<UsMeSCCPReferralRecord> = (
+  record
+) => {
+  if (!record) return;
+
+  const transformedRecord = cloneDeep(record) as UsMeSCCPReferralRecord;
+
+  transformedRecord.criteria.usMeServedXPortionOfSentence.eligibleDate = fieldToDate(
+    record.criteria.usMeServedXPortionOfSentence.eligibleDate
+  );
+
+  transformedRecord.criteria.usMeXMonthsRemainingOnSentence.eligibleDate = fieldToDate(
+    record.criteria.usMeXMonthsRemainingOnSentence.eligibleDate
+  );
+
+  return transformedRecord;
+};
