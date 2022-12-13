cb7c1db14160f5be14ffacd20ba63ffd59777891
Jen Overgaag <jovergaag@gmail.com>
2022-12-13 11:20:31 -0800

    ME workflows methodology (Recidiviz/recidiviz-dashboards#2809)
    GitOrigin-RevId: e568b5a3ed2ce4477073a3a0a0e281ae14f561c3

--- src/core/utils/constants.ts
+++ src/core/utils/constants.ts
@@ -71,4 +71,6 @@ export const WORKFLOWS_METHODOLOGY_URL: Record<string, string> = {
     "https://drive.google.com/file/d/1eHbSEOjjT9FvxffSbXOYEfOYPJheeu6t/view",
   US_ID:
     "https://drive.google.com/file/d/1pum9mrOIvGoBIwwE3dQEITod7O5mcYGm/view?usp=sharing",
+  US_ME:
+    "https://drive.google.com/file/d/1RIzASrkIaynsnUns8HGwyVxL8arqXlYH/view?usp=sharing",
 };
