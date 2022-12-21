c636bb0b4bcf1a98ba746272160b7e69cd97e2de
Anthony Kesich <anthony@recidiviz.org>
2022-12-21 14:21:01 -0600

    [Workflows] Extract form container into solo component (Recidiviz/recidiviz-dashboards#2847)
    GitOrigin-RevId: 007b1e6032c2c04a2afd63dc8aaf206d51ab4d91

---
+++ src/core/Paperwork/FormContainer.tsx
@@ -0,0 +1,134 @@
+/*
+ * Recidiviz - a data platform for criminal justice reform
+ * Copyright (C) 2022 Recidiviz, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * =============================================================================
+ */
+
+import {
+  Button,
+  palette,
+  Sans12,
+  Sans24,
+  spacing,
+} from "@recidiviz/design-system";
+import * as Sentry from "@sentry/react";
+import { rem, rgba } from "polished";
+import { useState } from "react";
+import styled from "styled-components/macro";
+
+import { OpportunityBase } from "../../WorkflowsStore/Opportunity/OpportunityBase";
+import { FormLastEdited } from "../FormLastEdited";
+
+const FormHeaderBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: ${rem(spacing.lg)} ${rem(spacing.xl)};
+  border-bottom: 1px solid ${palette.pine2};
+  margin-bottom: ${rem(spacing.lg)};
+  position: sticky;
+  top: 0;
+  background-color: ${palette.pine1};
+`;
+
+const FormHeaderSection = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${rem(spacing.md)};
+`;
+
+const FormHeading = styled(Sans24)`
+  color: ${palette.marble1};
+`;
+
+const LastEditedMessage = styled(Sans12)`
+  color: ${palette.marble1};
+  margin-top: ${rem(spacing.sm)};
+`;
+
+const DownloadButton = styled(Button).attrs({
+  kind: "primary",
+  shape: "block",
+})`
+  background-color: ${rgba(palette.marble1, 0.1)};
+  padding: ${rem(spacing.sm)} ${rem(spacing.md)};
+`;
+
+const FormContainerElement = styled.div`
+  background-color: ${palette.pine1};
+  color: ${palette.marble1};
+  min-height: 100vh;
+  padding-bottom: ${rem(spacing.xl)};
+`;
+
+const FormPreviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${rem(spacing.md)};
+  padding: ${rem(spacing.xl)} 10%;
+  width: 100%;
+`;
+
+type FormHeaderProps = {
+  agencyName: string;
+  heading: string;
+  onClickDownload: () => Promise<void>;
+  downloadButtonLabel: string;
+  opportunity: OpportunityBase<any, any>;
+  children: React.ReactNode;
+};
+
+export const FormContainer = ({
+  downloadButtonLabel,
+  heading,
+  onClickDownload,
+  agencyName,
+  opportunity,
+  children,
+}: FormHeaderProps) => {
+  const [isDownloading, setIsDownloading] = useState(false);
+
+  return (
+    <FormContainerElement>
+      <FormHeaderBar>
+        <FormHeaderSection>
+          <div>
+            <FormHeading>{heading}</FormHeading>
+            <LastEditedMessage>
+              <FormLastEdited agencyName={agencyName} form={opportunity.form} />
+            </LastEditedMessage>
+          </div>
+        </FormHeaderSection>
+        <FormHeaderSection>
+          <DownloadButton
+            disabled={isDownloading}
+            onClick={async () => {
+              setIsDownloading(true);
+              try {
+                await onClickDownload();
+              } catch (e) {
+                Sentry.captureException(e);
+              }
+              setIsDownloading(false);
+            }}
+          >
+            {isDownloading ? "Downloading..." : downloadButtonLabel}
+          </DownloadButton>
+        </FormHeaderSection>
+      </FormHeaderBar>
+      <FormPreviewContainer>{children}</FormPreviewContainer>
+    </FormContainerElement>
+  );
+};
--- src/core/Paperwork/US_ME/SCCP/FormSCCP.tsx
+++ src/core/Paperwork/US_ME/SCCP/FormSCCP.tsx
@@ -14,27 +14,17 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
-import {
-  Button,
-  palette,
-  Sans12,
-  Sans24,
-  spacing,
-} from "@recidiviz/design-system";
-import * as Sentry from "@sentry/react";
 import { runInAction, toJS } from "mobx";
 import { observer } from "mobx-react-lite";
-import { rem, rgba } from "polished";
-import { useState } from "react";
 import styled from "styled-components/macro";
 
 import { useRootStore } from "../../../../components/StoreProvider";
 import { Resident } from "../../../../WorkflowsStore/Resident";
-import { FormLastEdited } from "../../../FormLastEdited";
 import {
   downloadMultipleZipped,
   FileGeneratorArgs,
 } from "../../DOCXFormGenerator";
+import { FormContainer } from "../../FormContainer";
 import { connectComponentToOpportunityForm } from "../../OpportunityFormContext";
 import { REACTIVE_INPUT_UPDATE_DELAY } from "../../utils";
 import p1 from "./assets/p1.png";
@@ -43,52 +33,6 @@ import p3 from "./assets/p3.png";
 import p4 from "./assets/p4.png";
 import p5 from "./assets/p5.png";
 import p6 from "./assets/p6.png";
-
-const FormHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: ${rem(spacing.lg)} ${rem(spacing.xl)};
-  border-bottom: 1px solid ${palette.pine2};
-  margin-bottom: ${rem(spacing.lg)};
-`;
-
-const FormHeaderSection = styled.div`
-  display: flex;
-  align-items: center;
-  gap: ${rem(spacing.md)};
-`;
-
-const FormHeading = styled(Sans24)`
-  color: ${palette.marble1};
-`;
-
-const FormContainer = styled.div`
-  background-color: ${palette.pine1};
-  color: ${palette.marble1};
-  min-height: 100vh;
-  padding-bottom: ${rem(spacing.xl)};
-`;
-
-const LastEditedMessage = styled(Sans12)`
-  color: ${palette.marble1};
-  margin-top: ${rem(spacing.sm)};
-`;
-
-const DownloadButton = styled(Button).attrs({
-  kind: "primary",
-  shape: "block",
-})`
-  background-color: ${rgba(palette.marble1, 0.1)};
-  padding: ${rem(spacing.sm)} ${rem(spacing.md)};
-`;
-
-const FormPreviewContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: ${rem(spacing.md)};
-  padding: ${rem(spacing.xl)} 10%;
-  width: 100%;
-`;
 
 const FormPreviewPage = styled.img`
   height: auto;
@@ -135,8 +79,6 @@ const Form = observer(function FormSCCP() {
   const opportunity =
     workflowsStore?.selectedPerson?.verifiedOpportunities?.usMeSCCP;
 
-  const [isDownloading, setIsDownloading] = useState(false);
-
   if (!opportunity) {
     return null;
   }
@@ -144,42 +86,20 @@ const Form = observer(function FormSCCP() {
   const resident = opportunity.person;
 
   return (
-    <FormContainer>
-      <FormHeader>
-        <FormHeaderSection>
-          <div>
-            <FormHeading>SCCP Program Plan</FormHeading>
-            <LastEditedMessage>
-              <FormLastEdited agencyName="MDOC" form={opportunity.form} />
-            </LastEditedMessage>
-          </div>
-        </FormHeaderSection>
-        <FormHeaderSection>
-          <DownloadButton
-            disabled={isDownloading}
-            onClick={async () => {
-              setIsDownloading(true);
-              try {
-                await formDownloader(resident);
-              } catch (e) {
-                Sentry.captureException(e);
-              }
-              setIsDownloading(false);
-            }}
-          >
-            {isDownloading ? "Downloading..." : "Download .DOCX"}
-          </DownloadButton>
-        </FormHeaderSection>
-      </FormHeader>
-      <FormPreviewContainer>
-        {previewImages.map((imageUrl, index) => (
-          <FormPreviewPage
-            key={imageUrl}
-            src={imageUrl}
-            alt={`SCCP Program Plan preview, page ${index + 1}`}
-          />
-        ))}
-      </FormPreviewContainer>
+    <FormContainer
+      heading="SCCP Program Plan"
+      agencyName="MDOC"
+      downloadButtonLabel="Download .DOCX"
+      onClickDownload={() => formDownloader(resident)}
+      opportunity={opportunity}
+    >
+      {previewImages.map((imageUrl, index) => (
+        <FormPreviewPage
+          key={imageUrl}
+          src={imageUrl}
+          alt={`SCCP Program Plan preview, page ${index + 1}`}
+        />
+      ))}
     </FormContainer>
   );
 });
