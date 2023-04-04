4cbc9fbf2d465b732dda476a2dc50da74f9dedfc
Dana Hoffman <1130694+danawillow@users.noreply.github.com>
2023-04-03 20:31:12 -0400

    [Pathways] launch pathways new backend for all remaining metrics in prod (Recidiviz/recidiviz-dashboards#3236)
    GitOrigin-RevId: 13dbb31ec606e5204ffcd71c0b2a29f5812da18e

--- src/flags.js
+++ src/flags.js
@@ -4,15 +4,13 @@ export default process.env.REACT_APP_DEPLOY_ENV === "production"
       // TODO(395): Set to true when we have debugged the issues with the exit rate calculations
       enableRevocationRateByExit: false,
       enableVitalsGoalLine: false,
-      defaultMetricBackend: "OLD",
+      defaultMetricBackend: "NEW",
       metricBackendOverrides: {
-        // These need to be set so that PathwaysMetric actually hydrates (since the default is OLD)
-        prisonPopulationOverTime: "NEW",
-        supervisionPopulationOverTime: "NEW",
-        supervisionToLibertyOverTime: "NEW",
-        prisonToSupervisionPopulationOverTime: "NEW",
-        libertyToPrisonPopulationOverTime: "NEW",
-        supervisionToPrisonOverTime: "NEW",
+        // The new backend doesn't have officer names yet
+        supervisionToPrisonPopulationByOfficer: "OLD",
+        // The new backend doesn't handle projections yet
+        projectedPrisonPopulationOverTime: "OLD",
+        projectedSupervisionPopulationOverTime: "OLD",
       },
     }
   : process.env.REACT_APP_DEPLOY_ENV === "staging"
