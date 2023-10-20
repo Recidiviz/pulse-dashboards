dd2aa7b8cfda9c507d13a331726a0367de8d0ac7
Nnamdi Kenneth Ojibe <ken@recidiviz.org>
2023-10-20 17:39:21 -0400

    [Workflows] Configure Firebase to Automate Backups (Recidiviz/recidiviz-dashboards#4223)
    * Init firebase functions
    
    * gitignore env
    
    * set root eslint
    
    * Add "type" field to package.json
    
    - The "type" field with the value of "commonjs" has been added to the package.json file.
    
    * update package lock
    
    * The function now access google cloud secrets to get output dir
    
    * "Initiate configuration for Firestore export"
    
    This commit initiates the configuration process for exporting collections from Firestore. A configuration message is logged and displayed, showing the collections that will be exported from the specified project to the output bucket. The export is scheduled to run every Friday at 11:59 PM PDT.
    
    * change logging for style
    
    * var name fix
    
    * another style change
    
    * remove default from list of projects in `.firebaserc`
    
    * removing logging changes--original impl was correct.
    
    * refactor: Simplify code and improve error handling in export function
    
    This commit simplifies the code by removing unnecessary imports and comments. It also improves error handling by throwing a custom error with a specific error message when no collections are specified.
    
    The error message now includes the project ID and the output directory for better clarity. Additionally, a new file `utils.ts` is added to handle hold util functions
    
    * create index, etc.
    
    * remove secrets
    
    * change readme
    
    * readme edit
    
    * readme fix
    
    * fix
    
    * gitignore edit
    
    * readme fix
    
    * read me fix
    
    * Refactor configuration and export function in index.ts
    
    - Refactor the configuration code to improve readability and remove unnecessary variables.
    - Update the export function to use the correct output bucket.
    - Improve error handling and logging.
    - Provide more detailed success information in the logs.
    
    GitOrigin-RevId: beca7ee6ee3610d0a38a80a4ff3333917d7a9c57

---
+++ functions/src/index.ts
@@ -0,0 +1,101 @@
+// Recidiviz - a data platform for criminal justice reform
+// Copyright (C) 2023 Recidiviz, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// =============================================================================
+
+/* Firebase functions in Typescript documentation:
+ * https://firebase.google.com/docs/functions/typescript
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+import {v1} from "@google-cloud/firestore";
+
+import {pubsub} from "firebase-functions";
+import {info} from "firebase-functions/logger";
+import {throwErrorCustom} from "./utils";
+const client = new v1.FirestoreAdminClient();
+
+// Leave collectionIds empty to export all collections
+// or set to a list of collection IDs to export,
+// collectionIds: ['users', 'posts']
+const collectionIds = [
+  "milestoneMessage",
+  "configs",
+  "clientOpportunityUpdates",
+  "clientUpdatesV2",
+  "taskUpdates",
+  "userUpdates",
+];
+
+// CONFIGURATION
+info("CONFIGURATION INITIATED");
+
+// Getting project ID from environment variables
+const projectId = process.env.GCLOUD_PROJECT;
+if (!projectId) throwErrorCustom("❌ PROJECT_ID is not defined");
+
+// Getting the output bucket from environment variables
+const outputBucket = "recidiviz-dashboard-staging-firestore-backups";
+
+if (collectionIds.length === 0) {
+  throwErrorCustom(
+    `❌ No collections specified.
+     Specify the ${projectId} collections` +
+      `being exported to ${outputBucket}.`
+  );
+}
+
+// EXPORTING FUNCTION
+exports.scheduledFirestoreExport = pubsub
+  .schedule("every friday 23:59") // Runs every friday at 11:59 PM PDT
+  .onRun((context) => {
+    if (!outputBucket) {
+      throwErrorCustom("❌ OUTPUT_BUCKET is not defined");
+    }
+    // Logging the collections to be exported
+    const configurationMessage =
+      "⚙️ The following collections " +
+      `will be exported from ${projectId} to ${outputBucket}:
+    ${
+  collectionIds.map((collectionId) => `📁 ${collectionId}`).join("\n") +
+      "\n"
+}`;
+
+    info(configurationMessage);
+
+    // Defining the export function
+    const databaseName = client.databasePath(
+      projectId as unknown as string,
+      "(default)"
+    );
+
+    return client // Initiating the export operation
+      .exportDocuments({
+        name: databaseName,
+        outputUriPrefix: `gs://${outputBucket}`, // output destination
+        collectionIds: collectionIds, // collections to be exported
+      })
+      .then((responses) => {
+        const response = responses[0];
+        const successInfo =
+          "✅ SUCCESS: " +
+          `Collections exported successfully to ${outputBucket}
+          Operation Name: ${response["name"]}
+          Event ID: ${context.eventId}`;
+        info(successInfo);
+      })
+      .catch((err) => {
+        throwErrorCustom(`❌ Export operation failed:\n${err}`);
+      });
+  });
