82e7eec699d7c6b43fd3d541cc39a866b8c0d7ea
Gina Valderrama <40475205+Gina-Valderrama@users.noreply.github.com>
2024-08-02 17:20:45 -0700

    [Workflows][Single-instance forms] Migration script to move existing form updates to the new collection (Recidiviz/recidiviz-dashboards#5648)
    This PR adds a migration script at tools/migrate-updateForms.ts to move existing form edits (currently stored in clientOpportunityUpdates in firestore under the referralForm field) to the new clientFormUpdates collection. The script loops over all docs with an existing referralForm field, and then uses the opportunity name to determine the formId to be used in the new doc path (new paths will have the format: clientUpdatesV2/<client_id>/clientFormUpdates/<formId>).
    
    Tested locally via nx migrate-form-updates-local staff --update (after making some edits to offline forms) and also in staging with a dry run via nx migrate-form-updates-staging staff. Both times the logs printed as expected and I checked the firebase emulator locally to confirm the form edits migrated as expected.
    
    After this runs in prod and we've confirmed the changes look ok, we'll want to switch over to using this collection for form updates. We'll also follow up once we've switched over with a deletion migration to remove referralForm from clientOpportunityUpdates.
    
    GitOrigin-RevId: 240b3ee86a0c43dc7847be2c5b9668be944dc4d9

---
+++ apps/staff/tools/migrate-formUpdates.ts
@@ -0,0 +1,141 @@
+/* eslint-disable no-continue */
+/* eslint-disable no-console */
+// Recidiviz - a data platform for criminal justice reform
+// Copyright (C) 2024 Recidiviz, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// =============================================================================
+import { Firestore } from "@google-cloud/firestore";
+
+import { PartialRecord } from "../src/utils/typeUtils";
+import { OpportunityType } from "../src/WorkflowsStore";
+
+type Options = {
+  /** Dry run if not set */
+  shouldUpdate: boolean;
+};
+
+const formIdByOpp: PartialRecord<OpportunityType, string> = {
+  usPaAdminSupervision: "UsPaAdminSupervisionForm-usPaAdminSupervision",
+  usMiAddInPersonSecurityClassificationCommitteeReview:
+    "UsMiSCCReviewForm-common",
+  usMiSecurityClassificationCommitteeReview: "UsMiSCCReviewForm-common",
+  usMiWardenInPersonSecurityClassificationCommitteeReview:
+    "UsMiSCCReviewForm-common",
+  usTnExpiration: "UsTnExpirationForm-usTnExpiration",
+  usTnCustodyLevelDowngrade:
+    "UsTnReclassificationReviewForm-usTnCustodyLevelDowngrade",
+  usTnAnnualReclassification:
+    "UsTnReclassificationReviewForm-usTnAnnualReclassification",
+  usMeWorkRelease: "UsMeWorkReleaseForm-usMeWorkRelease",
+  usMeSCCP: "UsMeSCCPForm-usMeSCCP",
+  usMeFurloughRelease: "UsMeFurloughReleaseForm-usMeFurloughRelease",
+  usMeReclassificationReview:
+    "UsMeAnnualReclassificationReviewForm-usMeReclassificationReview",
+  earnedDischarge: "UsIdEarnedDischargeForm-earnedDischarge",
+  usCaSupervisionLevelDowngrade:
+    "UsCaSupervisionLevelDowngradeForm-usCaSupervisionLevelDowngrade",
+  LSU: "LSUForm-LSU",
+  earlyTermination: "EarlyTerminationForm-earlyTermination",
+  compliantReporting: "CompliantReportingForm-compliantReporting",
+};
+
+const { FIREBASE_PROJECT, FIREBASE_CREDENTIAL } = process.env;
+console.log({ FIREBASE_PROJECT, FIREBASE_CREDENTIAL });
+
+function getDb() {
+  const fsSettings: FirebaseFirestore.Settings = FIREBASE_CREDENTIAL
+    ? {
+        projectId: FIREBASE_PROJECT,
+        keyFilename: FIREBASE_CREDENTIAL,
+      }
+    : {
+        projectId: "demo-dev",
+        host: "localhost:8080",
+        credentials: {},
+        ssl: false,
+        keyFilename: "",
+        ignoreUndefinedProperties: true,
+      };
+
+  return new Firestore(fsSettings);
+}
+
+const db = getDb();
+
+const skippedOpps: PartialRecord<OpportunityType, number> = {};
+
+async function migrateFormUpdates({ shouldUpdate }: Options) {
+  const legacyFormUpdateDocs = (
+    await db
+      .collectionGroup("clientOpportunityUpdates")
+      .where("referralForm", "!=", null)
+      .get()
+  ).docs;
+
+  await Promise.all(
+    legacyFormUpdateDocs.map(async (legacySnapshot) => {
+      try {
+        const legacyDoc = legacySnapshot.data();
+        const { path } = legacySnapshot.ref;
+
+        // Legacy path:
+        // "clientUpdatesV2/client_id/clientOpportunityUpdates/usXxOpportunityType"
+        const client_id = path.split("/").slice(-3)[0];
+        const opportunityType = path.split("/").slice(-1)[0] as OpportunityType;
+        const newFormId = formIdByOpp[opportunityType];
+
+        if (newFormId === undefined) {
+          console.log("No formId configured for doc: ", path);
+          if (skippedOpps[opportunityType] !== undefined) {
+            // @ts-ignore
+            // eslint-disable-next-line no-plusplus
+            skippedOpps[opportunityType]++;
+          } else {
+            skippedOpps[opportunityType] = 1;
+          }
+          return;
+        }
+        const formUpdateData = legacyDoc.referralForm;
+
+        // New path:
+        // "clientUpdatesV2/client_id/clientFormUpdates/formId"
+        const newFormUpdatesRef = db.doc(
+          `clientUpdatesV2/${client_id}/clientFormUpdates/${newFormId}`,
+        );
+        const newSnapshot = await newFormUpdatesRef.get();
+        if (!newSnapshot.exists) {
+          if (shouldUpdate) {
+            await newFormUpdatesRef.set(formUpdateData);
+            console.log(`Updated ${newFormUpdatesRef.path}`);
+          } else {
+            console.log(`Would update ${newFormUpdatesRef.path}`);
+          }
+        }
+      } catch (e) {
+        console.log("Error migrating: ", legacySnapshot.ref.path);
+        console.log(e);
+      }
+    }),
+  );
+  console.log("Skipped updates for missing formId:\n", skippedOpps);
+}
+
+const opts = {
+  shouldUpdate: process.argv.includes("--update"),
+};
+
+if (!opts.shouldUpdate) console.log("To actually run updates, pass --update");
+
+migrateFormUpdates(opts);
