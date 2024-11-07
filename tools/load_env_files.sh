87e87c7b0d205fa2845e4bbc6d2c731e6de4b24c
Roshan A <roshan.agrawal95@gmail.com>
2024-11-07 18:46:30 -0500

    fix deploy env (Recidiviz/recidiviz-dashboards#6704)
    GitOrigin-RevId: d8ab44d4d5cf3c9304497b1357e1aea25b2085f1

--- tools/load_env_files.sh
+++ tools/load_env_files.sh
@@ -21,6 +21,6 @@ echo "${env}" > .env
 env_staging=$(gcloud secrets versions access latest --secret=$env_staging_file --project recidiviz-dashboard-staging)
 echo "${env_staging}" > .env.staging
 env_prod=$(gcloud secrets versions access latest --secret=$env_prod_file --project recidiviz-dashboard-staging)
-echo "${env_prod}" > .env.prod
+echo "${env_prod}" > .env.production
 env_test=$(gcloud secrets versions access latest --secret=$env_test_file --project recidiviz-dashboard-staging)
 echo "${env_test}" > .env.test
