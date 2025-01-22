fbb5d51121c78c6a40bd71bc19ceaaa0941b82b6
Roshan A <roshan.agrawal95@gmail.com>
2025-01-22 10:54:07 -0500

    bit of waiting (Recidiviz/recidiviz-dashboards#7171)
    GitOrigin-RevId: c0f982b2d5a3adf414f10698856f116bf067a6b1

--- tools/clone_sentencing_db.sh
+++ tools/clone_sentencing_db.sh
@@ -8,9 +8,14 @@ set -euo pipefail
 dbs="$(gcloud sql instances list --project recidiviz-dashboard-staging --filter s-db-preview-$VERSION)"
 
 if [[ -z $dbs ]]; then
+  # Clone the db if it doesn't already exist
   echo "No databases found. cloning database"
   gcloud sql instances clone sentencing-db s-db-preview-$VERSION --project recidiviz-dashboard-staging
   gcloud sql instances patch s-db-preview-$VERSION --project recidiviz-dashboard-staging \
     --no-deletion-protection \
     --availability-type=zonal
+else
+  # Wait for the clone operation to finish if it does already exist
+  CLONE_OPERATION_ID="$(gcloud sql operations list --project recidiviz-dashboard-staging --instance s-db-preview-$VERSION --filter "TYPE=CLONE" --format="get(NAME)")"
+  gcloud sql operations wait --project recidiviz-dashboard-staging $CLONE_OPERATION_ID
 fi
