9a66dd18d420b802abad324bcbd1073fcbae8063
Alexa Batino <37125652+alexabatino@users.noreply.github.com>
2025-01-27 12:07:33 -0500

    [JII Texts] Generate new app for JII texting server (Recidiviz/recidiviz-dashboards#7159)
    * Nx generation output; start README
    
    * Remove sensible plugin, simplify main app code
    
    * Copy project.json, config files from sentencing-server
    
    * Remove reference to non-existent library in test target (for now)
    
    * Exclude jii-texting-server test for now
    
    * Deleting targets covered by future PR; deleting other copy/generation artifacts
    
    GitOrigin-RevId: 3914c9b82dcfabfbdcb300a5c277a0158ebe5397

---
+++ apps/jii-texting-server/src/main.ts
@@ -0,0 +1,40 @@
+// Recidiviz - a data platform for criminal justice reform
+// Copyright (C) 2025 Recidiviz, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// =============================================================================
+
+import Fastify from "fastify";
+
+const host = process.env["HOST"] ?? "localhost";
+const port = process.env["PORT"] ? Number(process.env["PORT"]) : 3000;
+
+// Instantiate Fastify with some config
+const server = Fastify({
+  logger: true,
+});
+
+server.get("/", async function handler() {
+  return { hello: "world" };
+});
+
+// Start listening.
+server.listen({ port, host }, (err) => {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  } else {
+    console.log(`[ ready ] http://${host}:${port}`);
+  }
+});
