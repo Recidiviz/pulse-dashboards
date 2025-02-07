3e5d16cf902efae72c1c90ad727d10118ff9cc14
Michelle Orden <82831800+morden35@users.noreply.github.com>
2025-02-07 10:09:32 -0600

    [JII] Set Up JII Server Testing (Recidiviz/recidiviz-dashboards#7304)
    * make test folder and index file
    
    * fix import
    
    * fix import
    
    ---------
    
    Co-authored-by: Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
    GitOrigin-RevId: 4aa22a6e85b852c6757e3e1c540ee3a7e27af386

---
+++ apps/jii-texting-server/src/test/setup/index.ts
@@ -0,0 +1,49 @@
+// Recidiviz - a data platform for criminal justice reform
+// Copyright (C) 2025 Recidiviz, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// =============================================================================
+
+import { StateCode } from "@prisma/jii-texting-server/client";
+import { beforeAll, vi } from "vitest";
+
+import { getPrismaClientForStateCode } from "~@jii-texting-server/prisma";
+import { MockImportRoutesHandler } from "~fastify-data-import-plugin/testkit";
+import server from "~jii-texting-server/main";
+
+export const testPort = process.env["PORT"]
+  ? Number(process.env["PORT"])
+  : 3003;
+export const testHost = process.env["HOST"] ?? "localhost";
+
+export let testServer: typeof server;
+export const testPrismaClient = getPrismaClientForStateCode(StateCode.US_ID);
+
+vi.mock("~fastify-data-import-plugin", () => ({
+  ImportRoutesHandler: MockImportRoutesHandler,
+}));
+
+beforeAll(async () => {
+  testServer = server;
+
+  // Start listening.
+  testServer.listen({ port: testPort, host: testHost }, (err) => {
+    if (err) {
+      testServer.log.error(err);
+      process.exit(1);
+    } else {
+      console.log(`[ ready ] http://${testHost}:${testPort}`);
+    }
+  });
+});
