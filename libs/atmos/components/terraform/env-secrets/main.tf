d239fed4bb534d2d66d46b29a38a95960e6dc99b
Dan Hansen <dan@recidiviz.org>
2025-02-20 11:25:21 -0800

    [Atmos] Split `env_secrets` files by app (Recidiviz/recidiviz-dashboards#7416)
    * [Atmos] Split `env_secrets` files by app
    
    * update secrets; drop mega file
    
    GitOrigin-RevId: e75484433d11fcbf8e051e2f757eecfbae2e13e8

--- libs/atmos/components/terraform/env-secrets/main.tf
+++ libs/atmos/components/terraform/env-secrets/main.tf
@@ -1,16 +1,34 @@
 data "sops_file" "env" {
-  source_file = "./env-secrets.enc.yaml"
+  for_each = local.source_files
+  source_file = "./secrets/${each.value}"
 }
 
 locals {
-  # Create a map of nonsensitive map of keys from the SOPS file to use in for_each`
-  env_data = nonsensitive(toset([for k,v in yamldecode(data.sops_file.env.raw) : k]))
+  source_files = fileset("./secrets/", "*.enc.yaml")
+
+  env_secret_yamls = flatten([
+    for _, file in local.source_files : [
+      for k, v in yamldecode(data.sops_file.env[file].raw) : {
+        yaml_file = file
+        yaml_key  = nonsensitive(k)
+      }
+    ]
+  ])
+
+  env_secrets = {
+    for secret in local.env_secret_yamls : secret["yaml_key"] => secret
+  }
+
+  # Create a map of nonsensitive map of keys from the SOPS file to use in `for_each`
+  env_secret_names = nonsensitive(toset([
+    for key, _ in local.env_secrets: key
+  ]))
 }
 
 
 resource "google_secret_manager_secret" "env_secrets" {
-  for_each = local.env_data
-  secret_id = each.key
+  for_each = local.env_secret_names
+  secret_id = each.value
   replication {
     auto {
     }
@@ -18,11 +36,13 @@ resource "google_secret_manager_secret" "env_secrets" {
 }
 
 resource "google_secret_manager_secret_version" "env_secrets_version" {
-  for_each = local.env_data
+  for_each = local.env_secret_names
   secret = google_secret_manager_secret.env_secrets[each.key].name
   # Convert YAML dictionary to .env syntax
   secret_data = join("\n", [
-    for key, value in yamldecode(data.sops_file.env.raw)[each.key]: join("", [key, "=", value])
+    for key, value in yamldecode(
+      data.sops_file.env[local.env_secrets[each.value]["yaml_file"]].raw
+    )[each.key]: join("", [key, "=", value])
   ])
   deletion_policy = "DISABLE"
 }
