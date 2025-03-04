922d3d61a7575140741c16291beb247d9a66b095
Roshan A <roshan.agrawal95@gmail.com>
2025-03-04 12:02:49 -0500

    New Data Import Plugin that runs as a script (Recidiviz/recidiviz-dashboards#7520)
    * data import handler
    
    * did it
    
    * better type constraining
    
    * constrain file types as well
    
    * so close
    
    * more
    
    * maybe fin
    
    * env var
    
    * test config
    
    * done
    
    * simplify
    
    * fix env
    
    * new env vars
    
    * lint
    
    * remove package.json
    
    * ignore package.json
    
    * cleanup
    
    * maybe this will work
    
    * remove package json
    
    * undo mock extended
    
    * comment
    
    GitOrigin-RevId: d428a555a897e53f69a4e2e8c5be13c837c2b87b

---
+++ libs/common/data-import-plugin/src/index.ts
@@ -0,0 +1,50 @@
+// Recidiviz - a data platform for criminal justice reform
+// Copyright (C) 2024 Recidiviz, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// =============================================================================
+
+import { Storage } from "@google-cloud/storage";
+import readline from "readline";
+
+import { ImportHandlerBase } from "~data-import-plugin/common/classes";
+import { PrismaClient } from "~data-import-plugin/common/types";
+
+/**
+ * ImportHandler is a class that handles the import of data from GCS.
+ */
+export class ImportHandler<T extends PrismaClient, M> extends ImportHandlerBase<
+  T,
+  M
+> {
+  override async *getDataFromGCS(bucket: string, file: string) {
+    const storage = new Storage();
+
+    // The files are newline-delimited JSON, so we need to split them
+    const fileStream = storage.bucket(bucket).file(file).createReadStream();
+
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity,
+    });
+
+    for await (const line of rl) {
+      try {
+        yield JSON.parse(line);
+      } catch (e) {
+        throw new Error(`Error parsing JSON ${line}: ${e}`);
+      }
+    }
+  }
+}
