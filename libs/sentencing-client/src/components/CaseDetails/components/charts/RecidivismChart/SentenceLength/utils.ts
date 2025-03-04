6b0e342efe3f4337158c1b0f11ca0f48d084cb2b
Roshan A <roshan.agrawal95@gmail.com>
2025-03-04 13:05:38 -0500

    [Sentencing][US_ND] Update sentence length buckets (Recidiviz/recidiviz-dashboards#7515)
    * fix labels
    
    * fin
    
    * closer to the mocks
    
    * good catch Mahmoud
    
    GitOrigin-RevId: 4023e8a8c0eedfed61366f376e7d08aa28b08aad

--- libs/sentencing-client/src/components/CaseDetails/components/charts/RecidivismChart/SentenceLength/utils.ts
+++ libs/sentencing-client/src/components/CaseDetails/components/charts/RecidivismChart/SentenceLength/utils.ts
@@ -34,9 +34,9 @@ import {
   getSubtitleLsirScore,
 } from "../../common/utils";
 
-const PLOT_HEIGHT = 300;
+const PLOT_HEIGHT_MULTIPLIER = 70;
 const DOT_RADIUS = 13;
-const RECT_INSET = 20;
+const RECT_INSET = 16;
 
 export function getRecidivismPlotSubtitle(insight: CaseInsight) {
   const {
@@ -58,6 +58,13 @@ export function getRecidivismPlotSubtitle(insight: CaseInsight) {
 }
 
 const PLOT_MARGIN_LEFT = 80;
+
+function sentenceLengthLabelFilter(d: {
+  sentenceLengthBucketStart: number;
+  sentenceLengthBucketEnd: number;
+}) {
+  return d.sentenceLengthBucketStart === 0 && d.sentenceLengthBucketEnd === -1;
+}
 
 export function getRecidivismPlot(
   insight: CaseInsight,
@@ -86,19 +93,46 @@ export function getRecidivismPlot(
 
     return {
       name,
+      sentenceLengthBucketStart,
+      sentenceLengthBucketEnd,
       ...lastDataPoint,
     };
   });
 
+  // The order of the y domain should first be sentence type labels, then the sentence length labels ordered by start value
+  const yDomain = [
+    ...transformedSeries.filter(sentenceLengthLabelFilter).map((d) => d.name),
+    ...transformedSeries
+      .filter((d) => !sentenceLengthLabelFilter(d))
+      .sort((a, b) => a.sentenceLengthBucketStart - b.sentenceLengthBucketStart)
+      .map((d) => d.name),
+  ];
+
+  // Make the domain the closest 5% multiple on either end of the min and max values
+  const domainStart =
+    Math.floor(
+      Math.min(...transformedSeries.map((series) => series.lowerCI)) * 20,
+    ) * 5;
+  const domainEnd =
+    Math.ceil(
+      Math.max(...transformedSeries.map((series) => series.upperCI)) * 20,
+    ) * 5;
+
   return plot({
     width: plotWidth,
-    height: PLOT_HEIGHT,
+    height: 35 + PLOT_HEIGHT_MULTIPLIER * transformedSeries.length,
     marginLeft: PLOT_MARGIN_LEFT,
     color: {
       domain: [...Object.keys(SENTENCE_TYPE_TO_COLOR), "white"],
       range: [...Object.values(SENTENCE_TYPE_TO_COLOR), "#FFFFFF"],
     },
-    x: { percent: true },
+    x: {
+      percent: true,
+      domain: [domainStart, domainEnd],
+    },
+    y: {
+      domain: yDomain,
+    },
     marks: [
       axisY({
         label: null,
@@ -110,7 +144,7 @@ export function getRecidivismPlot(
         label: null,
         tickFormat: (d) => `${d}%`,
         tickSize: 0,
-        interval: 5,
+        interval: 10,
         color: "#001133B2",
         fontWeight: 400,
         fontSize: 14,
