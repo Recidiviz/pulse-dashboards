5df01b1ddf4be9f1429897059901878f57472b84
Alexa Batino <37125652+alexabatino@users.noreply.github.com>
2025-03-05 12:32:52 -0500

    [Atmos][JII Texts] Instantiate TwilioAPIClient in Cloud Run job (Recidiviz/recidiviz-dashboards#7512)
    * Add commander and extra typings
    
    * Use commander to define command line arguments
    
    * Instantiate TwilioAPIClient in Cloud Run job
    
    * Add target to run script locally
    
    * Add twilio secrets
    
    * Skip jii-texting-jobs in tests for now
    
    GitOrigin-RevId: b6e3a80b33bb45857196204670316794f1bc554e

--- apps/jii-texting-jobs/src/main.ts
+++ apps/jii-texting-jobs/src/main.ts
@@ -15,6 +15,28 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
 
+import { Command } from "@commander-js/extra-typings";
+import { StateCode } from "@prisma/jii-texting-server/client";
+
 import { processJii } from "~jii-texting-jobs/scripts/process-jii";
 
-processJii();
+// Define CLI
+const program = new Command()
+  .option("-s, --state-code <code>", "State code to run the Cloud Run job for")
+  .option(
+    "-d, --dry-run [run]",
+    "Whether or not this is a dry run; messages will not be sent if equal to `true`",
+    true,
+  );
+
+function main() {
+  program.parse();
+  const options = program.opts();
+
+  processJii({
+    stateCode: options.stateCode as unknown as StateCode,
+    dryRun: options.dryRun as unknown as boolean,
+  });
+}
+
+main();
