acae3d453ea0426bf2923428badf216943c83472
Roshan A <roshan.agrawal95@gmail.com>
2025-03-06 14:46:02 -0500

    [Sentencing] Put data import infra in terraform (only staging) (Recidiviz/recidiviz-dashboards#7576)
    * data import handler
    
    * did it
    
    * better type constraining
    
    * constrain file types as well
    
    * so close
    
    * more
    
    * maybe fin
    
    * env var
    
    * test config
    
    * done
    
    * simplify
    
    * fix env
    
    * new env vars
    
    * lint
    
    * remove package.json
    
    * ignore package.json
    
    * cleanup
    
    * maybe this will work
    
    * Revert "maybe this will work"
    
    This reverts commit c4954b0805cdec71cd2d1b7d9276c3175ccbc0e1.
    
    * undo mock extended
    
    * terraform
    
    * clean up envs
    
    * delete delete delete
    
    * fin
    
    * remove gitignore
    
    * bucket
    
    GitOrigin-RevId: 478aa2db1023f19f6543fe3e87e921457d89ca03

---
+++ libs/atmos/components/terraform/apps/sentencing/services.tf
@@ -0,0 +1,52 @@
+# Create a service account
+resource "google_service_account" "default" {
+  account_id   = "sentencing"
+  display_name = "Sentencing Service Account for data import and running the server"
+}
+
+# Grant cloud run invoker so the service account can view Cloud Run services
+resource "google_project_iam_member" "cloudrunviewer" {
+  project = var.project_id
+  role    = "roles/run.viewer"
+  member  = "serviceAccount:${google_service_account.default.email}"
+}
+
+# Grant cloud run job executor so the service account can execute Cloud Run jobs with env variable overrides
+resource "google_project_iam_member" "cloudrundjobexecutor" {
+  project = var.project_id
+  role    = "roles/run.jobsExecutorWithOverrides"
+  member  = "serviceAccount:${google_service_account.default.email}"
+}
+
+# Grant Workflows invoker so the service account can invoke Workflows
+resource "google_project_iam_member" "workflowsinvoker" {
+  project = var.project_id
+  role    = "roles/workflows.invoker"
+  member  = "serviceAccount:${google_service_account.default.email}"
+}
+
+# Grant Eventarc eventReceiver so the service account can receive events
+resource "google_project_iam_member" "eventarceventreceiver" {
+  project = var.project_id
+  role    = "roles/eventarc.eventReceiver"
+  member  = "serviceAccount:${google_service_account.default.email}"
+}
+
+# Grant Iam service account user so the service account can orchestrate the internal cloud run service accounts
+resource "google_project_iam_member" "iamserviceaccountuser" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.default.email}"
+}
+
+# Grant Cloud SQL client so the service account can connect to Cloud SQL
+resource "google_project_iam_member" "cloudsqlclient" {
+  project = var.project_id
+  role    = "roles/cloudsql.client"
+  member  = "serviceAccount:${google_service_account.default.email}"
+}
+
+resource "google_pubsub_topic" "sentencing_export_success_topic" {
+  name    = "sentencing_export_success"
+  project = var.project_id
+}
