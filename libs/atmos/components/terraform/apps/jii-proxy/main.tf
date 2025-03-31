7e5a3e13540d2f6d6f77f787676bb6432a161f3c
Dana Hoffman <1130694+danawillow@users.noreply.github.com>
2025-03-31 15:33:29 -0700

    set up cloud run for the nginx server with atmos (Recidiviz/recidiviz-dashboards#7862)
    * set up cloud run for the nginx server with atmos
    
    * fix stack name + add cloud run SA as artifact registry reader
    
    GitOrigin-RevId: 4279d70b640a6b5eadbc5de0655ca5a826d7b101

---
+++ libs/atmos/components/terraform/apps/jii-proxy/main.tf
@@ -0,0 +1,13 @@
+module "cloud-run" {
+  source = "../../vendor/cloud-run"
+
+  service_name = "jii-proxy-server"
+  location     = var.location
+  project_id   = var.project_id
+
+  containers = [
+    {
+      container_image = "${var.artifact_registry_repo}/jii-proxy-server:${var.server_version}"
+    }
+  ]
+}
---
+++ libs/atmos/components/terraform/apps/jii-proxy/variables.tf
@@ -0,0 +1,20 @@
+variable "project_id" {
+  type        = string
+  description = "The project that we are deploying the app to"
+}
+
+variable "location" {
+  type        = string
+  description = "The GCP location (us-east1, us-central1, etc) that we are deploying the service to"
+}
+
+variable "artifact_registry_repo" {
+  type        = string
+  description = "Artifact Registry repository to use for nginx"
+}
+
+variable "server_version" {
+  type        = string
+  description = "The version tag of the image that we are deploying"
+  default     = "latest"
+}
