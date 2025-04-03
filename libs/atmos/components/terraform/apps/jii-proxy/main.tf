f9e79d5e503ee982772f4b25ef34930c973e62e2
Dana Hoffman <1130694+danawillow@users.noreply.github.com>
2025-04-03 11:02:55 -0700

    add load balancer resources to jii proxy (Recidiviz/recidiviz-dashboards#7957)
    * vendor the lb-http TF module
    
    * add load balancer resources to jii proxy
    
    GitOrigin-RevId: b1151f0cc386bc0ad70bf3b5d34a680fba61cd01

--- libs/atmos/components/terraform/apps/jii-proxy/main.tf
+++ libs/atmos/components/terraform/apps/jii-proxy/main.tf
@@ -1,3 +1,20 @@
+# Recidiviz - a data platform for criminal justice reform
+# Copyright (C) 2025 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+
 module "cloud-run" {
   source = "../../vendor/cloud-run"
 
@@ -12,4 +29,46 @@ module "cloud-run" {
   ]
 
   members = var.members
+}
+
+resource "google_project_service" "compute" {
+  project            = var.project_id
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_compute_region_network_endpoint_group" "serverless_neg" {
+  depends_on = [google_project_service.compute]
+
+  name                  = "jii-proxy-neg"
+  network_endpoint_type = "SERVERLESS"
+  region                = var.location
+  project               = var.project_id
+  cloud_run {
+    service = module.cloud-run.service_name
+  }
+}
+
+module "load-balancer" {
+  source = "../../vendor/lb-http"
+
+  name    = "jii-proxy-lb"
+  project = var.project_id
+
+  backends = {
+    default = {
+      groups = [
+        {
+          group = google_compute_region_network_endpoint_group.serverless_neg.id
+        }
+      ]
+
+      enable_cdn = false
+
+      log_config = {
+        enable      = true
+        sample_rate = 1
+      }
+    }
+  }
 }
--- libs/atmos/components/terraform/apps/jii-proxy/outputs.tf
+++ libs/atmos/components/terraform/apps/jii-proxy/outputs.tf
@@ -15,28 +15,8 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
 
-variable "project_id" {
-  type        = string
-  description = "The project that we are deploying the app to"
-}
 
-variable "location" {
-  type        = string
-  description = "The GCP location (us-east1, us-central1, etc) that we are deploying the service to"
-}
-
-variable "artifact_registry_repo" {
-  type        = string
-  description = "Artifact Registry repository to use for nginx"
-}
-
-variable "server_version" {
-  type        = string
-  description = "The version tag of the image that we are deploying"
-  default     = "latest"
-}
-
-variable "members" {
-  type        = list(string)
-  description = "Users/SAs to be given invoker access to the service"
+output "external_ip" {
+  description = "The external IPv4 assigned to the load balancer"
+  value       = module.load-balancer.external_ip
 }
