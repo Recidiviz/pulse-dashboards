d706b8c9039f2227d0eaff4ee4262bfa6d4222c5
Dan Hansen <dan@recidiviz.org>
2025-05-07 15:04:47 -0700

    Update `postgres-bq-data-transfer` to configure transfers to regional and multi-regional datasets (Recidiviz/recidiviz-dashboards#8370)
    * Use multi-region location
    
    * use regional transfers
    
    ---------
    
    Co-authored-by: Alexa Batino <alexa@recidiviz.org>
    GitOrigin-RevId: f1a0569808275e2549aef32dd50855077639e55c

--- libs/atmos/components/terraform/postgres-bq-data-transfer/resources.tf
+++ libs/atmos/components/terraform/postgres-bq-data-transfer/resources.tf
@@ -4,12 +4,20 @@ resource "google_project_iam_member" "permissions" {
   member  = "serviceAccount:${var.service_account_email}"
 }
 
+resource "google_bigquery_dataset" "regional_transfer_dataset" {
+  for_each = var.postgresql.databases
+
+  dataset_id  = "${var.dataset_name}_${each.key}_regional"
+  description = "A regional copy of the sentencing database for state code ${each.key}"
+  location    = var.location
+}
+
 resource "google_bigquery_dataset" "transfer_dataset" {
   for_each = var.postgresql.databases
 
   dataset_id  = "${var.dataset_name}_${each.key}"
   description = "A copy of the sentencing database for state code ${each.key}"
-  location    = var.location
+  location    = "US"
 }
 
 resource "random_id" "attachment" {
@@ -32,16 +40,16 @@ resource "google_compute_network_attachment" "transfer_attachment" {
   }
 }
 
-resource "google_bigquery_data_transfer_config" "transfer_config" {
+resource "google_bigquery_data_transfer_config" "postgres_transfer_config" {
   for_each = var.postgresql.databases
 
-  display_name = "${var.dataset_name}_${each.key}"
+  display_name = "${var.dataset_name}_${each.key}_postgres"
   location     = var.location
   # This has to be a specific value for the connector to work
   # See https://github.com/hashicorp/terraform-provider-google/issues/19018
   data_source_id         = "postgresql"
-  schedule               = "every 24 hours"
-  destination_dataset_id = google_bigquery_dataset.transfer_dataset[each.key].dataset_id
+  schedule               = "every day 9:00"
+  destination_dataset_id = google_bigquery_dataset.regional_transfer_dataset[each.key].dataset_id
   project                = var.project_id
   service_account_name   = var.service_account_email
 
@@ -58,5 +66,19 @@ resource "google_bigquery_data_transfer_config" "transfer_config" {
       for _, asset in var.tables :
       "${each.key}/public/${asset}"
     ])
+  }
+}
+
+resource "google_bigquery_data_transfer_config" "transfer_config" {
+  for_each = var.postgresql.databases
+
+  display_name = "${var.dataset_name}_${each.key}_bigquery"
+  location     = "US"
+  schedule = "every day 9:30"
+  destination_dataset_id= google_bigquery_dataset.transfer_dataset[each.key].dataset_id
+  data_source_id="cross_region_copy"
+  params={
+      "source_project_id": var.project_id,
+      "source_dataset_id":  google_bigquery_dataset.regional_transfer_dataset[each.key].dataset_id
   }
 }
