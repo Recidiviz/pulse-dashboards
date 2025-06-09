ef99d815b002ebe394068ddfa0e9ae3d237257a6
Ian MacFarland <macfarlandian@users.noreply.github.com>
2025-06-09 12:29:29 -0700

    [US_MA] new schema for credit activity (Recidiviz/recidiviz-dashboards#8723)
    GitOrigin-RevId: f2f7d4e21c88ece41cedda14472b2c50aeb80014

--- libs/datatypes/src/people/Resident/US_MA/metadata/schema.ts
+++ libs/datatypes/src/people/Resident/US_MA/metadata/schema.ts
@@ -30,6 +30,34 @@ const nullZeroNumberSchema = z
   .nullable()
   .transform((i) => i ?? 0);
 
+const creditValueSchema = z
+  .number()
+  // this union and additional validation is needed because unfortunately
+  // the BigQuery JSON export renders integers as numbers and floats as strings
+  .or(z.string())
+  .nullish()
+  .transform((i) => i ?? 0)
+  .transform((input, ctx) => {
+    const output = Number(input);
+    if (isNaN(output)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Not a number",
+      });
+      return z.NEVER;
+    }
+    return output;
+  });
+
+const creditActivitySchema = z.object({
+  creditDate: dateStringSchema,
+  activity: z.string(),
+  rating: z.string().nullable(),
+  [usMaEarnedCreditType.enum.EARNED_GOOD_TIME]: creditValueSchema,
+  [usMaEarnedCreditType.enum.BOOST]: creditValueSchema,
+  [usMaEarnedCreditType.enum.COMPLETION]: creditValueSchema,
+});
+
 export const usMaResidentMetadataSchema = z.object({
   stateCode: z.literal("US_MA"),
   rtsDate: dateStringSchema.nullable(),
@@ -39,30 +67,7 @@ export const usMaResidentMetadataSchema = z.object({
   totalCompletionCreditDaysCalculated: nullZeroNumberSchema,
   totalStateCredit: nullZeroNumberSchema,
   totalStateCreditDaysCalculated: nullZeroNumberSchema,
-  creditActivity: z.array(
-    z.object({
-      creditDate: dateStringSchema,
-      creditType: usMaEarnedCreditType,
-      creditsEarned: z
-        .number()
-        .or(z.string())
-        .transform((input, ctx) => {
-          const output = Number(input);
-          if (isNaN(output)) {
-            ctx.addIssue({
-              code: z.ZodIssueCode.custom,
-              message: "Not a number",
-            });
-            return z.NEVER;
-          }
-          return output;
-        }),
-      activityCode: z.string(),
-      activity: z.string(),
-      activityType: z.string(),
-      rating: z.string().nullable(),
-    }),
-  ),
+  creditActivity: z.array(creditActivitySchema),
   lastUpdatedDate: dateStringSchema,
 });
 
@@ -70,6 +75,9 @@ export const usMaResidentMetadataSchema = z.object({
  * US_MA-specific data from the Recidiviz data platform about an incarcerated person
  */
 export type UsMaResidentMetadata = z.output<typeof usMaResidentMetadataSchema>;
+
+export type UsMaCreditActivity = z.infer<typeof creditActivitySchema>;
+
 /**
  * US_MA-specific data in its raw form, as stored in Firestore
  */
