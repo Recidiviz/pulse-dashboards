8304e0da002efbc1757a6626fd6d2bce939a70ea
Gina Valderrama <40475205+Gina-Valderrama@users.noreply.github.com>
2025-07-30 09:39:19 -0700

    [Insights][US_UT] Use supervisor label in the supervisor list header (Recidiviz/recidiviz-dashboards#9126)
    GitOrigin-RevId: f1d6df57fb29186e9e5b81828ce5f3a1c8d0923b

--- apps/staff/src/InsightsStore/presenters/SupervisionOfficerSupervisorsPresenter.ts
+++ apps/staff/src/InsightsStore/presenters/SupervisionOfficerSupervisorsPresenter.ts
@@ -140,7 +140,7 @@ export class SupervisionOfficerSupervisorsPresenter implements Hydratable {
   }
 
   get pageTitle(): string {
-    return "Select a supervisor to view their overview";
+    return `Select a ${this.labels.supervisionSupervisorLabel} to view their overview`;
   }
 
   trackViewed(): void {
