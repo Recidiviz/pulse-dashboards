635ea60530c583486505ea45790d259febac2f49
Roshan A <roshan.agrawal95@gmail.com>
2025-09-08 14:30:58 -0500

    Atmos setup for meeting asssitant backends (Recidiviz/recidiviz-dashboards#9555)
    GitOrigin-RevId: 78f22fbd0581cac8e2120eac54eb741361d8d41a

---
+++ libs/atmos/components/terraform/apps/meetings/main.tf
@@ -0,0 +1,242 @@
+data "sops_file" "env" {
+  source_file = "../../env-secrets/secrets/meetings.enc.yaml"
+}
+
+locals {
+  is_production = var.project_id == "recidiviz-dashboard-production"
+  env_secrets   = yamldecode(data.sops_file.env.raw)
+
+  shared_server_env = local.env_secrets["env_meetings_server"]
+  server_env        = local.env_secrets[var.server_env_key]
+
+  migrate_db_env = local.env_secrets[var.migrate_db_env_key]
+
+  shared_data_import_env = local.env_secrets["env_meetings_data_import"]
+  data_import_env        = var.configure_import ? local.env_secrets[var.data_import_env_key] : {}
+
+  server_image_name = "meetings-server"
+
+  migrate_db_image_name = "meetings-server"
+
+  import_image_name = "meetings-data-import"
+  import_job_name   = "meetings-data-import"
+
+  etl_bucket_name     = "meetings-etl-data"
+  archive_bucket_name = "${local.etl_bucket_name}-archive"
+
+  # This list needs to be marked as nonsensitive so it can be used in `for_each`
+  # the keys are not sensitive, so it is fine if they end up in the Terraform resource names
+  server_env_vars = nonsensitive([
+    for key, value in merge(local.shared_server_env, local.server_env) : {
+      # The values are sensitive so we want to omit them from the plans
+      value = sensitive(value)
+      name  = key
+    }
+  ])
+
+  # This list needs to be marked as nonsensitive so it can be used in `for_each`
+  # the keys are not sensitive, so it is fine if they end up in the Terraform resource names
+  migrate_db_env_vars = nonsensitive([
+    for key, value in local.migrate_db_env : {
+      # The values are sensitive so we want to omit them from the plans
+      value = sensitive(value)
+      name  = key
+    }
+  ])
+
+  # This list needs to be marked as nonsensitive so it can be used in `for_each`
+  # the keys are not sensitive, so it is fine if they end up in the Terraform resource names
+  data_import_env_vars = nonsensitive([
+    for key, value in merge(local.shared_data_import_env, local.data_import_env) : {
+      # The values are sensitive so we want to omit them from the plans
+      value = sensitive(value)
+      name  = key
+    }
+  ])
+}
+
+module "database" {
+  source = "../../vendor/cloud-sql-instance"
+
+  project_id                                    = var.project_id
+  create_bigquery_connection                    = false
+  instance_key                                  = var.sql_instance_name
+  base_secret_name                              = var.sql_base_secret_name
+  database_version                              = "POSTGRES_16"
+  has_readonly_user                             = false
+  availability_type                             = var.database_availability_type
+  region                                        = "us-central1"
+  zone                                          = "us-central1-f"
+  secondary_zone                                = var.database_secondary_zone
+  tier                                          = "db-custom-1-3840"
+  additional_databases                          = ["us_ne"]
+  private_network                               = var.private_network
+  enable_private_path_for_google_cloud_services = var.private_network != null ? true : false
+
+  insights_config = {
+    query_insights_enabled : true
+    query_string_length : 1024
+    record_application_tags : false
+    record_client_address : false
+  }
+}
+
+module "server" {
+  source = "../../vendor/cloud-run"
+
+  project_id      = var.project_id
+  location        = var.location
+  service_name    = var.server_name
+  service_account = google_service_account.default.email
+
+  containers = [
+    {
+      container_image = "${var.artifact_registry_repo}/${local.server_image_name}:${var.server_container_version}"
+
+      env_vars = local.server_env_vars
+
+      volume_mounts = [{
+        name       = "cloudsql"
+        mount_path = "/cloudsql"
+      }]
+    }
+  ]
+
+
+  volumes = [{
+    name = "cloudsql"
+    cloud_sql_instance = {
+      instances = [module.database.connection_name]
+    }
+    }
+  ]
+}
+
+# Configure a job that will migrate the database schema
+module "migrate-db-job" {
+  source                        = "../../vendor/cloud-run-job-exec"
+  exec                          = true
+  name                          = var.migrate_db_name
+  image                         = "${var.artifact_registry_repo}/${local.migrate_db_image_name}:${var.migrate_db_container_version}"
+  project_id                    = var.project_id
+  location                      = var.location
+  env_vars                      = local.migrate_db_env_vars
+  cloud_run_deletion_protection = false
+  service_account_email         = google_service_account.default.email
+  container_command             = ["./scripts/migrate-dbs.sh"]
+
+  volumes = [{
+    name = "cloudsql"
+    cloud_sql_instance = {
+      instances = [module.database.connection_name]
+    }
+    }
+  ]
+
+  volume_mounts = [{
+    name       = "cloudsql"
+    mount_path = "/cloudsql"
+  }]
+}
+
+# Configure a job that will import data
+# We don't execute it on deploy - it will be run when the workflow is triggered
+module "import-job" {
+  source = "../../vendor/cloud-run-job-exec"
+
+  # Don't create an import job for demo
+  count = var.configure_import ? 1 : 0
+
+  exec                          = false
+  name                          = local.import_job_name
+  image                         = "${var.artifact_registry_repo}/${local.import_image_name}:${var.import_container_version}"
+  project_id                    = var.project_id
+  location                      = var.location
+  env_vars                      = local.data_import_env_vars
+  cloud_run_deletion_protection = false
+  service_account_email         = google_service_account.default.email
+  timeout                       = "3600s"
+  max_retries                   = 1
+
+  volumes = [{
+    name = "cloudsql"
+    cloud_sql_instance = {
+      instances = [module.database.connection_name]
+    }
+    }
+  ]
+
+  volume_mounts = [{
+    name       = "cloudsql"
+    mount_path = "/cloudsql"
+  }]
+}
+
+module "gcs_bucket" {
+  source = "../../vendor/submodules/cloud-storage-bucket"
+
+  # Don't create a bucket for demo
+  count = var.configure_import ? 1 : 0
+
+  project_id = var.project_id
+  location   = var.location
+  prefix     = var.project_id
+  names      = [local.etl_bucket_name, local.archive_bucket_name]
+  logging = {
+    log_bucket = "${var.project_id}-gcs-object-logs"
+  }
+  versioning = {
+    "meetings-etl-data" = true
+  }
+  storage_class = "STANDARD"
+  lifecycle_rules = [{
+    action = {
+      type = "Delete"
+    }
+    condition = {
+      num_newer_versions = 3
+    }
+  }]
+  set_admin_roles = true
+  bucket_admins = {
+    (local.etl_bucket_name) = "serviceAccount:${var.project_number}-compute@developer.gserviceaccount.com,serviceAccount:${var.data_platform_project_number}-compute@developer.gserviceaccount.com"
+  }
+
+  set_viewer_roles = true
+  bucket_viewers = {
+    (local.archive_bucket_name) = "serviceAccount:cloud-build-ci-cd@${var.data_platform_project_id}.iam.gserviceaccount.com"
+  }
+}
+
+# Configure a Google Workflow that is executed when a pubsub notification
+module "handle-meetings-gcs-upload" {
+  source = "../../vendor/google-workflows-workflow"
+
+  # Don't create a workflow for demo
+  count = var.configure_import ? 1 : 0
+
+  project_id            = var.project_id
+  region                = var.location
+  service_account_email = google_service_account.default.email
+  workflow_name         = "handle-meetings-upload-wf"
+  workflow_trigger = {
+    event_arc = {
+      name                  = "handle-meetings-upload-wf"
+      service_account_email = google_service_account.default.email
+      pubsub_topic_id       = google_pubsub_topic.meetings_export_success_topic[0].id
+      matching_criteria = [
+        {
+          attribute = "type"
+          value     = "google.cloud.pubsub.topic.v1.messagePublished"
+        }
+      ]
+    }
+  }
+  workflow_source = file("${path.module}/workflows/handle-meetings-gcs-upload.workflows.yaml")
+  env_vars = {
+    PROJECT_ID        = var.project_id
+    JOB_NAME          = module.import-job[0].id
+    ARCHIVE_BUCKET_ID = module.gcs_bucket[0].names[local.archive_bucket_name]
+    ETL_BUCKET_ID     = module.gcs_bucket[0].names[local.etl_bucket_name]
+  }
+}
---
+++ libs/atmos/components/terraform/apps/meetings/variables.tf
@@ -0,0 +1,121 @@
+variable "project_id" {
+  type        = string
+  description = "The project that we are deploying the app to"
+}
+
+variable "project_number" {
+  type        = string
+  description = "The project number for the project we are deploying to"
+}
+
+variable "data_platform_project_number" {
+  type        = string
+  description = "The data platform project number for the equivalent project/env we are deploying to"
+}
+
+variable "data_platform_project_id" {
+  type        = string
+  description = "The data platform project id for the equivalent project/env we are deploying to"
+}
+
+variable "location" {
+  type        = string
+  description = "The GCP location (us-east1, us-central1, etc) that we are deploying the service to"
+}
+
+variable "configure_import" {
+  type        = bool
+  description = "Whether to include the import-related resources"
+  default     = true
+}
+
+variable "sql_instance_name" {
+  type        = string
+  description = "The name of the SQL instance"
+  default     = "meetings"
+}
+
+variable "sql_base_secret_name" {
+  type        = string
+  description = "The name of the SQL secret prefixes"
+  default     = "meetings"
+}
+
+variable "database_availability_type" {
+  type        = string
+  description = "The availability type for the service"
+  default     = null
+}
+
+variable "database_secondary_zone" {
+  type        = string
+  description = "The secondary zone for the service"
+  default     = null
+}
+
+variable "private_network" {
+  type        = string
+  description = "(Optional_ The private network to use for the SQL instance"
+  default     = null
+}
+
+variable "server_name" {
+  type        = string
+  description = "The name of the SQL instance"
+  default     = "meetings-server"
+}
+
+variable "artifact_registry_repo" {
+  type        = string
+  description = "Artifact Registry repository to use for Meeting Assistant"
+}
+
+variable "artifact_registry_project_id" {
+  type        = string
+  description = "The project id the Artifact Registry repository is in"
+}
+
+variable "server_container_version" {
+  type        = string
+  description = "The version tag of the image that will deployed for the server"
+  default     = "latest"
+}
+
+variable "service_account_id" {
+  type        = string
+  description = "The name of the service account"
+  default     = "meetings"
+}
+
+variable "server_env_key" {
+  type        = string
+  description = "The key for the server env"
+}
+
+variable "migrate_db_name" {
+  type        = string
+  description = "The name of the SQL instance"
+  default     = "meetings-migrate-db"
+}
+
+variable "migrate_db_container_version" {
+  type        = string
+  description = "The version tag of the image that will be used for the database migration job"
+  default     = "latest"
+}
+variable "migrate_db_env_key" {
+  type        = string
+  description = "The key for the migrate db env"
+}
+
+variable "data_import_env_key" {
+  type        = string
+  description = "The key for the import data env"
+  default     = null
+}
+
+variable "import_container_version" {
+  type        = string
+  description = "The version tag of the image that will deployed for the import job"
+  default     = "latest"
+}
