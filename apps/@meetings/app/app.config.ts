8b58afe71ca3635cf72432e4e0637bfef5cf5f4d
Dana Hoffman <1130694+danawillow@users.noreply.github.com>
2026-04-14 09:36:59 -0700

    [Meetings] remove tablet support (Recidiviz/recidiviz-dashboards#13119)
    ## Description of the change
    
    We don't have any plans for tablet users in the near future, so we don't
    need to let people download on iPads.
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    | --------------------------- |
    ---------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes #XXXX
    
    Resolve Sentry issue:
    Fixes RECIDIVIZ-PULSE-DASHBOARD-[XXX]
    
    ## Checklists
    
    ### Development
    
    These boxes should be checked by the submitter prior to merging:
    
    - [ ] Manual testing against realistic data has been performed locally
    - [ ] E2E have been run for Lantern changes ([Steps in the
    Readme](https://github.com/Recidiviz/recidiviz-dashboards#running-e2e-tests))
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    GitOrigin-RevId: bbde76e9bc755b9406a754c2df56bd1be3b2f3b5

--- apps/@meetings/app/app.config.ts
+++ apps/@meetings/app/app.config.ts
@@ -80,7 +80,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
     ios: {
-      supportsTablet: true,
+      supportsTablet: false,
       bundleIdentifier: bundleIdentifier,
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
