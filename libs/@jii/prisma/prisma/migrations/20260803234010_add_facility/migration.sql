c7d2880395e5f5ac643095edeab54cddce7f854b
Ian MacFarland <macfarlandian@users.noreply.github.com>
2026-08-04 12:37:51 -0700

    [JII] add facility model (Recidiviz/recidiviz-dashboards#14987)
    ## Description of the change
    
    Prisma schema update and migration for IncarcerationFacility
    
    ## Type of change
    
    > All pull requests must have at least one of the following labels
    applied (otherwise the PR will fail):
    
    | Label | Description |
    | --------------------------- |
    ---------------------------------------------------------------------------------------------------------
    |
    | Type: Bug | non-breaking change that fixes an issue |
    | Type: Feature | non-breaking change that adds functionality |
    | Type: Breaking Change | fix or feature that would cause existing
    functionality to not work as expected |
    | Type: Non-breaking refactor | change addresses some tech debt item or
    prepares for a later change, but does not change functionality |
    | Type: Configuration Change | adjusts configuration to achieve some end
    related to functionality, development, performance, or security |
    | Type: Dependency Upgrade | upgrades a project dependency - these
    changes are not included in release notes |
    
    ## Related issues
    
    Closes OBT-29543
    
    ## Checklists
    
    ### Development
    
    These boxes should be checked by the submitter prior to merging:
    
    - [x] Manual testing against realistic data has been performed locally
    - [ ] E2E have been run for Lantern changes ([Steps in the
    Readme](https://github.com/Recidiviz/recidiviz-dashboards#running-e2e-tests))
    
    ### Code review
    
    These boxes should be checked by reviewers prior to merging:
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer
    - [ ] Potential security implications or infrastructural changes have
    been considered, if relevant
    
    ---
    
    <sub>Stack created with <a
    href="https://github.com/github/gh-stack">GitHub Stacks CLI</a> • <a
    href="https://gh.io/stacks-feedback">Give Feedback 💬</a></sub>
    GitOrigin-RevId: d0618e2af1737a29fa55bdd2e7cd21dac8b50a77

---
+++ libs/@jii/prisma/prisma/migrations/20260803234010_add_facility/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "public"."IncarcerationFacility" (
+    "importedAt" TIMESTAMP(3) NOT NULL,
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "IncarcerationFacility_pkey" PRIMARY KEY ("id")
+);
